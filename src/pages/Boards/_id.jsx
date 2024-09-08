95d1cc8e1907ed43684c41dbf3bd58c17b70001c
long565 <nguyenduclong5a1@gmail.com>
2024-09-09 01:26:55 +0700

    axios init get method

--- src/pages/Boards/_id.jsx
+++ src/pages/Boards/_id.jsx
@@ -3,13 +3,28 @@ import AppBar from '~/components/AppBar/AppBar'
 import BoardBar from './BoardBar/BoardBar'
 import BoardContent from './BoardContent/BoardContent'
 import { mockData } from '~/apis/mock-data'
+import { useState } from 'react'
+import { useEffect } from 'react'
+import { fetchBoardDetailsAPI } from '~/apis'
 
 function Board() {
+  const [board, setBoard] = useState(null)
+
+  useEffect(() => {
+    //now we use fixed board id for testing, later we will get it from route params using react-router-dom
+    const boardId = '66ddda8ffd8934aed4269f64'
+
+    //call API to fetch board details
+    fetchBoardDetailsAPI(boardId).then((board) => {
+      setBoard(board)
+    })
+  }, [])
+
   return (
     <Container disableGutters maxWidth={false} sx={{ height: '100vh' }}>
       <AppBar />
-      <BoardBar board={mockData?.board} />
-      <BoardContent board={mockData?.board} />
+      <BoardBar board={board} />
+      <BoardContent board={board} />
     </Container>
   )
 }
