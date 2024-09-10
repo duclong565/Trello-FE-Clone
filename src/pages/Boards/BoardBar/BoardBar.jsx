57defc8b898a2df8a69634837d1a212c4b99836a
long565 <nguyenduclong5a1@gmail.com>
2024-09-10 23:45:37 +0700

    changes for aggregation of BE

--- src/pages/Boards/BoardBar/BoardBar.jsx
+++ src/pages/Boards/BoardBar/BoardBar.jsx
@@ -12,7 +12,6 @@ import Button from '@mui/material/Button'
 import PersonAddIcon from '@mui/icons-material/PersonAdd'
 import { capitalizeFirstLetter } from '~/utils/formatter'
 
-
 const MENU_STYLE = {
   color: 'white',
   backgroundColor: 'transparent',
@@ -20,42 +19,47 @@ const MENU_STYLE = {
   paddingX: '5px',
   borderRadius: '4px',
   '.MuiSvgIcon-root': {
-    color: 'white'
+    color: 'white',
   },
   '&:hover': {
-    backgroundColor: 'primary.50'
-  }
+    backgroundColor: 'primary.50',
+  },
 }
 
 function BoardBar({ board }) {
   return (
-    <Box sx={{
-      // display: { xs: 'none', md: 'flex', gap: 1 },
-      width: '100%',
-      height: (theme) => theme.trello.boardBarHeight,
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'space-between',
-      gap: 2,
-      padding: 2,
-      overflowX: 'auto',
-      bgcolor: (theme) => (theme.palette.mode === 'dark' ? '#34495e' : '#1976d2')
-
-    }}>
-
+    <Box
+      sx={{
+        // display: { xs: 'none', md: 'flex', gap: 1 },
+        width: '100%',
+        height: (theme) => theme.trello.boardBarHeight,
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        gap: 2,
+        padding: 2,
+        overflowX: 'auto',
+        bgcolor: (theme) =>
+          theme.palette.mode === 'dark' ? '#34495e' : '#1976d2',
+      }}
+    >
       <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
-        <Chip
-          sx={MENU_STYLE}
-          icon={<DashboardIcon />}
-          label={board?.title}
-          clickable
-        />
-        <Chip
-          sx={MENU_STYLE}
-          icon={<VpnLockIcon />}
-          label={capitalizeFirstLetter(board?.type)}
-          clickable
-        />
+        <Tooltip title={board?.description}>
+          <Chip
+            sx={MENU_STYLE}
+            icon={<DashboardIcon />}
+            label={board?.title}
+            clickable
+          />
+        </Tooltip>
+        <Tooltip title="D bt la cai j nhung co hinh trai dat">
+          <Chip
+            sx={MENU_STYLE}
+            icon={<VpnLockIcon />}
+            label={capitalizeFirstLetter(board?.type)}
+            clickable
+          />
+        </Tooltip>
         <Chip
           sx={MENU_STYLE}
           icon={<AddToDriveIcon />}
@@ -83,7 +87,7 @@ function BoardBar({ board }) {
           sx={{
             color: 'white',
             borderColor: 'white',
-            '&:hover': { borderColor: 'white' }
+            '&:hover': { borderColor: 'white' },
           }}
         >
           Invite
@@ -100,49 +104,84 @@ function BoardBar({ board }) {
               border: 'none',
               color: 'white',
               cursor: 'pointer',
-              '&:first-of-type': { bgcolor: '#a4b0de' }
-            }
+              '&:first-of-type': { bgcolor: '#a4b0de' },
+            },
           }}
         >
-          <Tooltip title="Quang2802">
-            <Avatar alt="Quang2802" src="https://scontent.fhan5-10.fna.fbcdn.net/v/t39.30808-1/448961556_1703774890365742_6186321680900832134_n.jpg?stp=dst-jpg_p200x200&_nc_cat=101&ccb=1-7&_nc_sid=0ecb9b&_nc_ohc=pRBLkZ0kC5EQ7kNvgGa6ls4&_nc_ht=scontent.fhan5-10.fna&oh=00_AYDQLyKz0bmrfnO0BGEkpVLQatgYRVGBUe0pRFLz5box1Q&oe=66B822E1" />
+          <Tooltip title="long565">
+            <Avatar
+              alt="long565"
+              src="https://scontent.fhan5-10.fna.fbcdn.net/v/t39.30808-1/448961556_1703774890365742_6186321680900832134_n.jpg?stp=dst-jpg_p200x200&_nc_cat=101&ccb=1-7&_nc_sid=0ecb9b&_nc_ohc=pRBLkZ0kC5EQ7kNvgGa6ls4&_nc_ht=scontent.fhan5-10.fna&oh=00_AYDQLyKz0bmrfnO0BGEkpVLQatgYRVGBUe0pRFLz5box1Q&oe=66B822E1"
+            />
           </Tooltip>
-          <Tooltip title="Quang2802">
-            <Avatar alt="Quang2802" src="https://scontent.fhan5-10.fna.fbcdn.net/v/t39.30808-1/448961556_1703774890365742_6186321680900832134_n.jpg?stp=dst-jpg_p200x200&_nc_cat=101&ccb=1-7&_nc_sid=0ecb9b&_nc_ohc=pRBLkZ0kC5EQ7kNvgGa6ls4&_nc_ht=scontent.fhan5-10.fna&oh=00_AYDQLyKz0bmrfnO0BGEkpVLQatgYRVGBUe0pRFLz5box1Q&oe=66B822E1" />
+          <Tooltip title="long565">
+            <Avatar
+              alt="long565"
+              src="https://scontent.fhan5-10.fna.fbcdn.net/v/t39.30808-1/448961556_1703774890365742_6186321680900832134_n.jpg?stp=dst-jpg_p200x200&_nc_cat=101&ccb=1-7&_nc_sid=0ecb9b&_nc_ohc=pRBLkZ0kC5EQ7kNvgGa6ls4&_nc_ht=scontent.fhan5-10.fna&oh=00_AYDQLyKz0bmrfnO0BGEkpVLQatgYRVGBUe0pRFLz5box1Q&oe=66B822E1"
+            />
           </Tooltip>
-          <Tooltip title="Quang2802">
-            <Avatar alt="Quang2802" src="https://scontent.fhan5-10.fna.fbcdn.net/v/t39.30808-1/448961556_1703774890365742_6186321680900832134_n.jpg?stp=dst-jpg_p200x200&_nc_cat=101&ccb=1-7&_nc_sid=0ecb9b&_nc_ohc=pRBLkZ0kC5EQ7kNvgGa6ls4&_nc_ht=scontent.fhan5-10.fna&oh=00_AYDQLyKz0bmrfnO0BGEkpVLQatgYRVGBUe0pRFLz5box1Q&oe=66B822E1" />
+          <Tooltip title="long565">
+            <Avatar
+              alt="long565"
+              src="https://scontent.fhan5-10.fna.fbcdn.net/v/t39.30808-1/448961556_1703774890365742_6186321680900832134_n.jpg?stp=dst-jpg_p200x200&_nc_cat=101&ccb=1-7&_nc_sid=0ecb9b&_nc_ohc=pRBLkZ0kC5EQ7kNvgGa6ls4&_nc_ht=scontent.fhan5-10.fna&oh=00_AYDQLyKz0bmrfnO0BGEkpVLQatgYRVGBUe0pRFLz5box1Q&oe=66B822E1"
+            />
           </Tooltip>
-          <Tooltip title="Quang2802">
-            <Avatar alt="Quang2802" src="https://scontent.fhan5-10.fna.fbcdn.net/v/t39.30808-1/448961556_1703774890365742_6186321680900832134_n.jpg?stp=dst-jpg_p200x200&_nc_cat=101&ccb=1-7&_nc_sid=0ecb9b&_nc_ohc=pRBLkZ0kC5EQ7kNvgGa6ls4&_nc_ht=scontent.fhan5-10.fna&oh=00_AYDQLyKz0bmrfnO0BGEkpVLQatgYRVGBUe0pRFLz5box1Q&oe=66B822E1" />
+          <Tooltip title="long565">
+            <Avatar
+              alt="long565"
+              src="https://scontent.fhan5-10.fna.fbcdn.net/v/t39.30808-1/448961556_1703774890365742_6186321680900832134_n.jpg?stp=dst-jpg_p200x200&_nc_cat=101&ccb=1-7&_nc_sid=0ecb9b&_nc_ohc=pRBLkZ0kC5EQ7kNvgGa6ls4&_nc_ht=scontent.fhan5-10.fna&oh=00_AYDQLyKz0bmrfnO0BGEkpVLQatgYRVGBUe0pRFLz5box1Q&oe=66B822E1"
+            />
           </Tooltip>
-          <Tooltip title="Quang2802">
-            <Avatar alt="Quang2802" src="https://scontent.fhan5-10.fna.fbcdn.net/v/t39.30808-1/448961556_1703774890365742_6186321680900832134_n.jpg?stp=dst-jpg_p200x200&_nc_cat=101&ccb=1-7&_nc_sid=0ecb9b&_nc_ohc=pRBLkZ0kC5EQ7kNvgGa6ls4&_nc_ht=scontent.fhan5-10.fna&oh=00_AYDQLyKz0bmrfnO0BGEkpVLQatgYRVGBUe0pRFLz5box1Q&oe=66B822E1" />
+          <Tooltip title="long565">
+            <Avatar
+              alt="long565"
+              src="https://scontent.fhan5-10.fna.fbcdn.net/v/t39.30808-1/448961556_1703774890365742_6186321680900832134_n.jpg?stp=dst-jpg_p200x200&_nc_cat=101&ccb=1-7&_nc_sid=0ecb9b&_nc_ohc=pRBLkZ0kC5EQ7kNvgGa6ls4&_nc_ht=scontent.fhan5-10.fna&oh=00_AYDQLyKz0bmrfnO0BGEkpVLQatgYRVGBUe0pRFLz5box1Q&oe=66B822E1"
+            />
           </Tooltip>
-          <Tooltip title="Quang2802">
-            <Avatar alt="Quang2802" src="https://scontent.fhan5-10.fna.fbcdn.net/v/t39.30808-1/448961556_1703774890365742_6186321680900832134_n.jpg?stp=dst-jpg_p200x200&_nc_cat=101&ccb=1-7&_nc_sid=0ecb9b&_nc_ohc=pRBLkZ0kC5EQ7kNvgGa6ls4&_nc_ht=scontent.fhan5-10.fna&oh=00_AYDQLyKz0bmrfnO0BGEkpVLQatgYRVGBUe0pRFLz5box1Q&oe=66B822E1" />
+          <Tooltip title="long565">
+            <Avatar
+              alt="long565"
+              src="https://scontent.fhan5-10.fna.fbcdn.net/v/t39.30808-1/448961556_1703774890365742_6186321680900832134_n.jpg?stp=dst-jpg_p200x200&_nc_cat=101&ccb=1-7&_nc_sid=0ecb9b&_nc_ohc=pRBLkZ0kC5EQ7kNvgGa6ls4&_nc_ht=scontent.fhan5-10.fna&oh=00_AYDQLyKz0bmrfnO0BGEkpVLQatgYRVGBUe0pRFLz5box1Q&oe=66B822E1"
+            />
           </Tooltip>
-          <Tooltip title="Quang2802">
-            <Avatar alt="Quang2802" src="https://scontent.fhan5-10.fna.fbcdn.net/v/t39.30808-1/448961556_1703774890365742_6186321680900832134_n.jpg?stp=dst-jpg_p200x200&_nc_cat=101&ccb=1-7&_nc_sid=0ecb9b&_nc_ohc=pRBLkZ0kC5EQ7kNvgGa6ls4&_nc_ht=scontent.fhan5-10.fna&oh=00_AYDQLyKz0bmrfnO0BGEkpVLQatgYRVGBUe0pRFLz5box1Q&oe=66B822E1" />
+          <Tooltip title="long565">
+            <Avatar
+              alt="long565"
+              src="https://scontent.fhan5-10.fna.fbcdn.net/v/t39.30808-1/448961556_1703774890365742_6186321680900832134_n.jpg?stp=dst-jpg_p200x200&_nc_cat=101&ccb=1-7&_nc_sid=0ecb9b&_nc_ohc=pRBLkZ0kC5EQ7kNvgGa6ls4&_nc_ht=scontent.fhan5-10.fna&oh=00_AYDQLyKz0bmrfnO0BGEkpVLQatgYRVGBUe0pRFLz5box1Q&oe=66B822E1"
+            />
           </Tooltip>
-          <Tooltip title="Quang2802">
-            <Avatar alt="Quang2802" src="https://scontent.fhan5-10.fna.fbcdn.net/v/t39.30808-1/448961556_1703774890365742_6186321680900832134_n.jpg?stp=dst-jpg_p200x200&_nc_cat=101&ccb=1-7&_nc_sid=0ecb9b&_nc_ohc=pRBLkZ0kC5EQ7kNvgGa6ls4&_nc_ht=scontent.fhan5-10.fna&oh=00_AYDQLyKz0bmrfnO0BGEkpVLQatgYRVGBUe0pRFLz5box1Q&oe=66B822E1" />
+          <Tooltip title="long565">
+            <Avatar
+              alt="long565"
+              src="https://scontent.fhan5-10.fna.fbcdn.net/v/t39.30808-1/448961556_1703774890365742_6186321680900832134_n.jpg?stp=dst-jpg_p200x200&_nc_cat=101&ccb=1-7&_nc_sid=0ecb9b&_nc_ohc=pRBLkZ0kC5EQ7kNvgGa6ls4&_nc_ht=scontent.fhan5-10.fna&oh=00_AYDQLyKz0bmrfnO0BGEkpVLQatgYRVGBUe0pRFLz5box1Q&oe=66B822E1"
+            />
           </Tooltip>
-          <Tooltip title="Quang2802">
-            <Avatar alt="Quang2802" src="https://scontent.fhan5-10.fna.fbcdn.net/v/t39.30808-1/448961556_1703774890365742_6186321680900832134_n.jpg?stp=dst-jpg_p200x200&_nc_cat=101&ccb=1-7&_nc_sid=0ecb9b&_nc_ohc=pRBLkZ0kC5EQ7kNvgGa6ls4&_nc_ht=scontent.fhan5-10.fna&oh=00_AYDQLyKz0bmrfnO0BGEkpVLQatgYRVGBUe0pRFLz5box1Q&oe=66B822E1" />
+          <Tooltip title="long565">
+            <Avatar
+              alt="long565"
+              src="https://scontent.fhan5-10.fna.fbcdn.net/v/t39.30808-1/448961556_1703774890365742_6186321680900832134_n.jpg?stp=dst-jpg_p200x200&_nc_cat=101&ccb=1-7&_nc_sid=0ecb9b&_nc_ohc=pRBLkZ0kC5EQ7kNvgGa6ls4&_nc_ht=scontent.fhan5-10.fna&oh=00_AYDQLyKz0bmrfnO0BGEkpVLQatgYRVGBUe0pRFLz5box1Q&oe=66B822E1"
+            />
           </Tooltip>
-          <Tooltip title="Quang2802">
-            <Avatar alt="Quang2802" src="https://scontent.fhan5-10.fna.fbcdn.net/v/t39.30808-1/448961556_1703774890365742_6186321680900832134_n.jpg?stp=dst-jpg_p200x200&_nc_cat=101&ccb=1-7&_nc_sid=0ecb9b&_nc_ohc=pRBLkZ0kC5EQ7kNvgGa6ls4&_nc_ht=scontent.fhan5-10.fna&oh=00_AYDQLyKz0bmrfnO0BGEkpVLQatgYRVGBUe0pRFLz5box1Q&oe=66B822E1" />
+          <Tooltip title="long565">
+            <Avatar
+              alt="long565"
+              src="https://scontent.fhan5-10.fna.fbcdn.net/v/t39.30808-1/448961556_1703774890365742_6186321680900832134_n.jpg?stp=dst-jpg_p200x200&_nc_cat=101&ccb=1-7&_nc_sid=0ecb9b&_nc_ohc=pRBLkZ0kC5EQ7kNvgGa6ls4&_nc_ht=scontent.fhan5-10.fna&oh=00_AYDQLyKz0bmrfnO0BGEkpVLQatgYRVGBUe0pRFLz5box1Q&oe=66B822E1"
+            />
           </Tooltip>
-          <Tooltip title="Quang2802">
-            <Avatar alt="Quang2802" src="https://scontent.fhan5-10.fna.fbcdn.net/v/t39.30808-1/448961556_1703774890365742_6186321680900832134_n.jpg?stp=dst-jpg_p200x200&_nc_cat=101&ccb=1-7&_nc_sid=0ecb9b&_nc_ohc=pRBLkZ0kC5EQ7kNvgGa6ls4&_nc_ht=scontent.fhan5-10.fna&oh=00_AYDQLyKz0bmrfnO0BGEkpVLQatgYRVGBUe0pRFLz5box1Q&oe=66B822E1" />
+          <Tooltip title="long565">
+            <Avatar
+              alt="long565"
+              src="https://scontent.fhan5-10.fna.fbcdn.net/v/t39.30808-1/448961556_1703774890365742_6186321680900832134_n.jpg?stp=dst-jpg_p200x200&_nc_cat=101&ccb=1-7&_nc_sid=0ecb9b&_nc_ohc=pRBLkZ0kC5EQ7kNvgGa6ls4&_nc_ht=scontent.fhan5-10.fna&oh=00_AYDQLyKz0bmrfnO0BGEkpVLQatgYRVGBUe0pRFLz5box1Q&oe=66B822E1"
+            />
           </Tooltip>
-          <Tooltip title="Quang2802">
-            <Avatar alt="Quang2802" src="https://scontent.fhan5-10.fna.fbcdn.net/v/t39.30808-1/448961556_1703774890365742_6186321680900832134_n.jpg?stp=dst-jpg_p200x200&_nc_cat=101&ccb=1-7&_nc_sid=0ecb9b&_nc_ohc=pRBLkZ0kC5EQ7kNvgGa6ls4&_nc_ht=scontent.fhan5-10.fna&oh=00_AYDQLyKz0bmrfnO0BGEkpVLQatgYRVGBUe0pRFLz5box1Q&oe=66B822E1" />
+          <Tooltip title="long565">
+            <Avatar
+              alt="long565"
+              src="https://scontent.fhan5-10.fna.fbcdn.net/v/t39.30808-1/448961556_1703774890365742_6186321680900832134_n.jpg?stp=dst-jpg_p200x200&_nc_cat=101&ccb=1-7&_nc_sid=0ecb9b&_nc_ohc=pRBLkZ0kC5EQ7kNvgGa6ls4&_nc_ht=scontent.fhan5-10.fna&oh=00_AYDQLyKz0bmrfnO0BGEkpVLQatgYRVGBUe0pRFLz5box1Q&oe=66B822E1"
+            />
           </Tooltip>
         </AvatarGroup>
       </Box>
-
     </Box>
   )
 }
